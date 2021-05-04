81d161dbb9276e88d96cd2c847b260b0bfe21659
karadenizli-koefte <karadenizli-koefte-esgir2326@hotmail.de>
2021-05-04 10:28:01 +0200

    Chapter 6 Exercises 1 - 3

---
+++ Functional Programming in CSharp/FunctionalProgrammingExercises4/FPLibrary/EitherExtensions.cs
@@ -0,0 +1,34 @@
+﻿using LaYumba.Functional;
+using System;
+using static LaYumba.Functional.F;
+
+namespace FPLibrary
+{
+    public static class EitherExtensions
+    {
+        public static Option<R> ToOption<L, R>(this Either<L, R> either)
+            => either.Match(
+                l => None,
+                r => Some(r));
+
+        public static Either<L, R> ToEither<L, R>(this Option<R> opt, L left)
+            => opt.Match<Either<L, R>>(
+                () => Left(left),
+                r => Right(r));
+
+        public static Either<L, R> ToEither<L, R>(this Option<R> opt, Func<L> left)
+            => opt.Match<Either<L, R>>(
+                () => left(),
+                r => r);
+
+        public static Option<RR> Bind<L, R, RR>(this Either<L, R> either, Func<R, Option<RR>> func)
+            => either.Match(
+                Left: l => None,
+                Right: r => func(r));
+
+        public static Option<RR> Bind<L, R, RR>(this Option<R> opt, Func<R, Either<L, RR>> func)
+            => opt.Match(
+                None: () => None,
+                Some: s => func(s).ToOption());
+    }
+}
