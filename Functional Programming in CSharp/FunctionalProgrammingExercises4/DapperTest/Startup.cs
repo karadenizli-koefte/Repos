568167d306f479584f7865a1692d2b0c49c7cc69
karadenizli-koefte <karadenizli-koefte-esgir2326@hotmail.de>
2021-05-11 08:52:58 +0200

    Use Dapper for CRUD operations

--- Functional Programming in CSharp/FunctionalProgrammingExercises4/DapperTest/Startup.cs
+++ Functional Programming in CSharp/FunctionalProgrammingExercises4/DapperTest/Startup.cs
@@ -28,7 +28,7 @@ namespace DapperTest
         {
             services.AddDbContext<ApplicationDbContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<ICompanyRepository, CompanyRepositoryEF>();
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
             services.AddControllersWithViews();
         }
 
