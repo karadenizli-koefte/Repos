b8f72cba4e0635186a172710a2b14ce377691b67
Karadenizli Koefte <karadenizli-koefte-esgir2326@hotmail.com>
2021-06-05 22:52:16 +0200

    OpenCV - 2D Discrete Fourier Transformation for Edge Detection

--- Image_Processing/Image Processing/Contours.py
+++ Image_Processing/Image Processing/Contours.py
@@ -8,21 +8,30 @@ img = cv2.imread(imgPath)
 img = cv2.resize(img, (int(img.shape[1] / 4), int(img.shape[0] / 4)))
 
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+ret, thresh = cv2.threshold(imgray, 240, 245, 0)
+kernel = np.ones((5, 5), np.uint8)
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # Draw all contours
-# img = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+#img = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
 # To draw an individual contour, say 4th contour
 # img = cv2.drawContours(img, contours, 3, (0, 255, 0), 3)
 
 # But most of the time, below method will be useful
-cnt = contours[4]
+print(len(contours))
+cnt = contours[0]
 img = cv2.drawContours(img, [cnt], 0, (0, 255, 0), 3)
+
+cv2.namedWindow('res2')
+cv2.imshow('res2', thresh)
+cv2.waitKey()
 
 cv2.namedWindow('res')
 cv2.imshow('res', img)
 cv2.waitKey()
+
 cv2.destroyAllWindows()
--- Image_Processing/Image Processing/ImageLoader.py
+++ Image_Processing/Image Processing/ImageLoader.py
@@ -7,3 +7,11 @@ def load_img(path, scale):
     img = cv2.resize(img, (int(img.shape[1] / scale), int(img.shape[0] / scale)))
 
     return img
+
+
+def load_img_gray(path, scale):
+    # Load image
+    img = cv2.imread(path, 0)
+    img = cv2.resize(img, (int(img.shape[1] / scale), int(img.shape[0] / scale)))
+
+    return img
