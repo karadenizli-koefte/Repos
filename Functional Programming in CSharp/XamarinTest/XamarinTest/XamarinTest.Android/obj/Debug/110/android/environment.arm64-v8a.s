0189b076236056853d3cfd86918cadf9eb327ce2
Karadenizli Koefte <karadenizli-koefte-esgir2326@hotmail.com>
2021-06-22 19:56:00 +0200

    Install Android Emulator

--- Functional Programming in CSharp/XamarinTest/XamarinTest/XamarinTest.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ Functional Programming in CSharp/XamarinTest/XamarinTest/XamarinTest.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"2facffac-bd7b-4754-9954-c0b9813d2214"
+	.asciz	"b38c59a0-9c13-4c81-90ac-aeb2abb63b1c"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
