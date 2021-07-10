6010902830eaf80f7dfd3a5b6e67bbfc652f155f
Karadenizli Koefte <karadenizli-koefte-esgir2326@hotmail.com>
2021-07-10 22:37:04 +0200

    Microsoft.ML - Initial commit + Read data from CSV file and return list

---
+++ Machine_and_Deep_Learning/TurkishPoliticalOpinionsPrediction/TurkishPoliticalOpinionsPrediction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using TurkishPoliticalViewsPredictor;
+using TurkishPoliticalViewsPredictor.Model;
+
+namespace TurkishPoliticalOpinionsPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Class1.LoadData();
+
+            var count = data.Count();
+            var row = data.ElementAt(0);
+            Console.WriteLine("Number of rows = " +  count);
+        }
+    }
+}
---
+++ Machine_and_Deep_Learning/TurkishPoliticalOpinionsPrediction/TurkishPoliticalViewsPredictor/Class1.cs
@@ -0,0 +1,22 @@
+﻿using LanguageExt;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using TurkishPoliticalViewsPredictor.Model;
+
+namespace TurkishPoliticalViewsPredictor
+{
+    public static class Class1
+    {
+        public static IEnumerable<PoliticalPersonRaw> LoadData()
+        {
+            //Create MLContext
+            MLContext mlContext = new MLContext();
+
+            //Load Data
+            IDataView data = mlContext.Data.LoadFromTextFile<PoliticalPersonRaw>(@"Dataset/data.csv", separatorChar: ',', hasHeader: true);
+
+            return mlContext.Data.CreateEnumerable<PoliticalPersonRaw>(data, reuseRowObject: true);
+        }
+    }
+}
